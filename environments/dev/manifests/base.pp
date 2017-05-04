76a18ce21887370f1c4d0274a34d5088f3a8d593
Tim Neumann <tn@move-elevator.de>
2017-05-04 22:46:22 +0200

    removed public key

--- environments/dev/manifests/base.pp
+++ environments/dev/manifests/base.pp
@@ -12,9 +12,6 @@ accounts::user { 'sandbox':
 		'adm'
 	],
 	shell => '/bin/bash',
-	sshkeys => [
-		'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDATSSkpu6/7yPAAuXZSVrrrjCyxSk2jgmyZY2vA+mi/FLrel1cMOnDlCjjYK0KH3WBz4sH4zVNJT+AaxHa5EfGUerj/Lz3+bf5AFgQSWMH/TAJBZkTH3GgI0GLVvapmwNHtGaV7L7BB39gS5/UG60yDuD+qXVSfj+mPorFPyUTALexrO30qWLABGJpIbKzmN62iyE7f2Cw2/53uPTTvmYj7b6EZP82baAJT1o81aGs9BDjyRwDXZ/AFHZz8HmbgMk199z20ReTAZpALYhYnnRopHxP2eYRWxTLpnFm3UCgN/7Fv8Dxj2BAx1PFof4eZiY5zZPLY1y25ZcnKlATgqub t.neumann@mac-11.elevator.local'
-	],
 	password => '$6$FsvJM0mx$MkRjvOTE/Sxyv7PeXFU5WGUQ2GgyiYTNpjdbB3wdOgiuTeWTUR.MRXyprrW585XIE/Njt/MjseSJNnHm82uDO0',
 }
 
